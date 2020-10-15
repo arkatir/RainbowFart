2adf91e0a08acf909d4c329f1294da233321969b
guillaume <guillaume.pangaud@gmail.com>
2020-10-15 11:45:32 +0200

    Trampoline sound (broken)

---
+++ Assets/Aurélien/Scripts/Trampoline.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    //Audio parameters
+    [SerializeField] private AudioSource bouncy_s;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        bouncy_s.Play();
+    }
+
+}
